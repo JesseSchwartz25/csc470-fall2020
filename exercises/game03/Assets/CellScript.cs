2e7f6368de525c93cabe5cdbc138ccb3c15edbc6
Jesse Schwartz <js9764a@student.american.edu>
2020-10-01 00:01:05 -0400

    first build of conway game

---
+++ exercises/game03/Assets/CellScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellScript : MonoBehaviour
+{
+	Renderer rend;
+	public Color aliveColor;
+	public Color deadColor;
+    public Color toDie;
+    public PlayerController PC;
+
+	public int x = -1;
+	public int y = -1;
+    bool beenSelected = false;
+
+	public bool nextAlive;
+	private bool _alive = false;
+    public bool Alive
+    {
+        get
+        {
+            return this._alive;
+        }
+        set
+        {
+            this._alive = value;
+            
+            if (this._alive) {
+				rend.material.color = aliveColor;
+			} else {
+				rend.material.color = deadColor;
+			}
+		}
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		rend = gameObject.GetComponent<Renderer>();
+        //this.Alive = Random.value < 0.5f;
+        this.Alive = false;
+
+        GameObject Player = GameObject.Find("Player");
+        PC = Player.GetComponent<PlayerController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!this.nextAlive && Alive)
+        {
+            rend.material.color = toDie;
+        }
+
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(PC.counter <= 10)
+    //    {
+    //        this.Alive = true;
+    //    }
+    //    Debug.Log(this.Alive);
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (PC.counter <= 5 && !beenSelected)
+        {
+            this.Alive = true;
+            beenSelected = true;
+            PC.counter++;
+        }
+        Debug.Log(this.Alive);
+    }
+
+
+    public void TurnRed()
+    {
+        if (!this.nextAlive && this._alive)
+        {
+            rend.material.color = toDie;
+            Debug.Log("to die to die to die");
+        }
+    }
+}
---
+++ exercises/game03/Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    int speed = 5;
+    public int counter = 0;
+    public Rigidbody rb = new Rigidbody();
+    bool canJump = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(transform.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(transform.forward * Time.deltaTime * -1 * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            rb.AddForce(0, 200, 0);
+            canJump = false;
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canJump = true;
+        //counter++;
+        Debug.Log(counter);
+    }
+}
